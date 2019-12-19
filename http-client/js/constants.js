9f2211bf7f59fb63692b9b6ad0398ba16f7d06e4
695199262 <695199262@qq.com>
2019-12-20 05:23:54 +0800

    [FINISH] Ready to Produce

--- http-client/js/constants.js
+++
@@ -1,4 +0,0 @@
-const SEND_HOST = "http://localhost:8080";
-const RTMP_HOST = "rtmp://localhost";
-const FLV_LIVE_HOST = "http://192.168.142.20";
-const DANMAKU_WS_HOST = "ws://localhost:8080";
---
+++ src/main/resources/static/js/constants.js
@@ -0,0 +1,20 @@
+
+const SEND_HOST = "";
+const RTMP_HOST = "rtmp://fs2.lzfnb.top";
+const FLV_LIVE_HOST = "";
+const DANMAKU_WS_HOST = "ws://fs2.lzfnb.top:8080";
+
+
+/*
+
+const SEND_HOST = "http://fs2.lzfnb.top:8080";
+const RTMP_HOST = "rtmp://fs2.lzfnb.top:8080";
+const FLV_LIVE_HOST = "http://fs2.lzfnb.top";
+const DANMAKU_WS_HOST = "ws://fs2.lzfnb.top:8080";*/
+
+/*
+
+const SEND_HOST = "http://localhost:8080";
+const RTMP_HOST = "rtmp://192.168.142.20";
+const FLV_LIVE_HOST = "http://192.168.142.20";
+const DANMAKU_WS_HOST = "ws://localhost:8080";*/
